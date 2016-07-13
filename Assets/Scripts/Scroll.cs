9b4982ea1fb6013e6869629f67c71a3036f6686b
Rubin Deliallisi <r.deliallisi@jacobs-university.de>
2016-07-13 20:57:05 +0200

    Added background and fixed monkey animation

---
+++ Assets/Scripts/Scroll.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scroll : MonoBehaviour {
+
+    public float speed;
+
+    private MeshRenderer rend;
+    private Vector2 offset;
+
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<MeshRenderer>();
+        offset = new Vector2(0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        offset.y = Time.time * speed;
+        rend.material.mainTextureOffset = offset;
+	}
+}
