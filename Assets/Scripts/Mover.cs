bab465266e1f8ddb02152e1008365cb18ed1ab2d
JHalili <johana.halili@gmail.com>
2016-07-13 22:16:49 +0200

    No conflicts please

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+    private Rigidbody2D rigidBody;
+    private Vector2 direction;
+    public float speed;
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody2D>();
+        direction = new Vector2(0, 1);
+        rigidBody.velocity = direction;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+    public float rotationSpeed;
+    private Rigidbody2D rigidBody;
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody2D>();
+        rigidBody.angularVelocity = rotationSpeed;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
